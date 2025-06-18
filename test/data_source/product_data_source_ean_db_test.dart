f0423461894ef6632a169813682a6d0a4386b9f1
Marcus Duarte <marcusedu@hotmail.com>
2025-06-17 23:37:15 -0300

    refactor tests to remove hardcoded auth tokens and unnecessary print statements

--- test/data_source/product_data_source_ean_db_test.dart
+++ test/data_source/product_data_source_ean_db_test.dart
@@ -7,7 +7,7 @@ void main() {
     final response = await productDataSource.barcodeLookup(
       barcode: '1234567890123',
     );
-    print(response);
+
     expect(response, isNotNull);
     expect(response.data, isNull);
     expect(response.error, isNotNull);
@@ -19,13 +19,9 @@ void main() {
     final productDataSource = ProductDataSourceEanDb();
     final response = await productDataSource.barcodeLookup(
       barcode: '7896051135425',
-      authToken:
-          'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIwYzJjOTE2Zi0zNDIyLTRmZjctODk3Ny00MzU'
-          '3ZTczOTBiM2QiLCJpc3MiOiJjb20uZWFuLWRiIiwiaWF0IjoxNzUwMjAxMTA2LCJleHA'
-          'iOjE3ODE3MzcxMDYsImlzQXBpIjoidHJ1ZSJ9.sH8caiWb7BgXe7b1z53HN18BErpRWd'
-          'YuksA5fwY6kDzbrqHqTBqp9V7ee2DFTPsl1Kx4tozY_LKOEeiYksQEmw',
+      authToken: 'TOKEN_HERE',
     );
-    print(response);
+
     expect(response, isNotNull);
     expect(response.data, isNotNull);
     expect(response.error, isNull);
@@ -36,11 +32,7 @@ void main() {
     final productDataSource = ProductDataSourceEanDb();
     final response = await productDataSource.barcodeLookup(
       barcode: '7896051135425Err',
-      authToken:
-          'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIwYzJjOTE2Zi0zNDIyLTRmZjctODk3Ny00MzU'
-          '3ZTczOTBiM2QiLCJpc3MiOiJjb20uZWFuLWRiIiwiaWF0IjoxNzUwMjAxMTA2LCJleHA'
-          'iOjE3ODE3MzcxMDYsImlzQXBpIjoidHJ1ZSJ9.sH8caiWb7BgXe7b1z53HN18BErpRWd'
-          'YuksA5fwY6kDzbrqHqTBqp9V7ee2DFTPsl1Kx4tozY_LKOEeiYksQEmw',
+      authToken: 'TOKEN_HERE',
     );
 
     expect(response, isNotNull);
